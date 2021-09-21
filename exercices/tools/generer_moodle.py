b425d36557b41a05523288dcb92973d0b90cab9c
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2021-09-22 01:13:40 +0200

    Update generer_moodle.py

--- exercices/tools/generer_moodle.py
+++ exercices/tools/generer_moodle.py
@@ -1,7 +1,15 @@
 import importlib.util
 import os
 
+import sys
+from pathlib import Path
+
 os.chdir("..")
+
+sys.path.insert(0, os.path.join(Path(__file__), ".."))
+
+def ss(*args) :
+    pass
 
 def module_from_file(module_name, file_path):
     spec = importlib.util.spec_from_file_location(module_name, file_path)
