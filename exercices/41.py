961e261d3a580ec36e418e40af047d721df86feb
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2021-11-16 00:52:52 +0100

    41

--- exercices/41.py
+++ exercices/41.py
@@ -1,9 +1,16 @@
 from libs.alien import *
 
-haut(5)
+a = 0
 
-while colonne() < 15 :
-    droite()
-    bas()
+haut(6)
+droite(6)
+while colonne() > 6 :
+    bas(3)
+    gauche(2)
+haut(2)
+droite(3)
+while colonne() < 14 :
+    droite(3)
+    haut(2)
 
 ss()
