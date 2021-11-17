b7c166b44c66880fc51402fa9294a4bfbabc9bdf
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2021-11-18 00:39:35 +0100

    Amélioration exo 42

--- exercices/42.py
+++ exercices/42.py
@@ -2,11 +2,13 @@ from libs.alien import *
 
 bas(7)
 gauche(7)
-haut(12)
-for loop in range(5) :
+haut(14)
+for loop in range(4) :
     while colonne() < 13 :
         droite(2)
     bas(2)
     gauche(10)
+    bas()
+gauche()
 
 ss()
